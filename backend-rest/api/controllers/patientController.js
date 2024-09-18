dd7e70296212a9d7c65706c753738b7603f5736d
jtonyNet <jtony.net@gmail.com>
2024-09-18 10:40:34 -0300

    feat(patients): teste green para rota de obter paciente

--- backend-rest/api/controllers/patientController.js
+++ backend-rest/api/controllers/patientController.js
@@ -17,16 +17,44 @@ class PatientController {
       res.status(StatusCodes.CREATED).json(newPatient);
 
     } catch (error) {
+      // log(error)
       res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ message: 'Error creating patient' });
     }
   }
 
   static async retrievePatientsList(req, res) {
     const patientsList = await patients.findAll({
-      attributes: ['uuid', 'name', 'phone', 'email', 'birth_date', 'gender', 'height', 'weight'],
+      attributes: [
+        'uuid', 'name', 'phone',
+        'email', 'birth_date', 'gender',
+        'height', 'weight'
+      ],
     })
 
     res.status(StatusCodes.OK).json(patientsList);
+  }
+
+  static async retrievePatientByUUID(req, res) {
+    const { uniqueId } = req.params
+
+    try {
+      const patient = await patients.findOne({
+        where: {
+          uuid: uniqueId,
+        },
+        attributes: [
+          'uuid', 'name', 'phone',
+          'email', 'birth_date', 'gender',
+          'height', 'weight'
+        ],
+      })
+
+      res.status(StatusCodes.OK).json(patient);
+    } catch (error) {
+      // log(error)
+      res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ message: 'Error retrieve patient' });
+    }
+
   }
 }
 
--- backend-rest/api/routes/patientRoutes.js
+++ backend-rest/api/routes/patientRoutes.js
@@ -6,5 +6,6 @@ const router = Router()
 router
   .post('/patients', PatientController.createPatient)
   .get('/patients', PatientController.retrievePatientsList)
+  .get('/patients/:uniqueId', PatientController.retrievePatientByUUID)
 
 module.exports = router
