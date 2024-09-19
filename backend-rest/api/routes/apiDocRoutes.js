4954f2b0caa99da3ed744367e5a9901713ca9c49
jtonyNet <jtony.net@gmail.com>
2024-09-19 16:22:12 -0300

    feat(apidocs): utilizando anotations para gerar o swagger

--- backend-rest/api/routes/apiDocRoutes.js
+++ backend-rest/api/routes/apiDocRoutes.js
@@ -1,11 +1,30 @@
 const { Router } = require('express')
 const swaggerUi = require('swagger-ui-express');
-const swaggerDocument = require('../swaggerDocument.json')
+const swaggerJsDoc = require('swagger-jsdoc');
+const path = require('path');
 
 const router = Router()
 
-router
-    .use('/docs', swaggerUi.serve)
-    .get('/docs', swaggerUi.setup(swaggerDocument));
+const swaggerOptions = {
+    definition: {
+        openapi: '3.0.0',
+        info: {
+            title: 'MedPlanner Rest API',
+            version: '0.0.6',
+            description: 'API documentation',
+        },
+        servers: [
+            {
+                url: 'http://localhost:3000',
+                description: 'Local Development server',
+            },
+        ],
+    },
+    apis: [path.join(__dirname, './patientRoutes.js')],
+};
+
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+router.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+
 
 module.exports = router
--- backend-rest/api/routes/patientRoutes.js
+++ backend-rest/api/routes/patientRoutes.js
@@ -6,11 +6,39 @@ const router = Router()
 
 router.use(authenticate)
 
+/**
+ * @swagger
+ * /patients:
+ *   get:
+ *     summary: Retrieve a list of patients
+ *     responses:
+ *       200:
+ *         description: A list of patients
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 type: object
+ *                 properties:
+ *                   uuid:
+ *                     type: string
+ *                     example: "69be741b-3bf4-41a2-9b44-0e8b655a54dd"
+ *                   name:
+ *                     type: string
+ *                     example: "Pedro Prado"
+ *                   email:
+ *                     type: string
+ *                     example: "pedro@xmail.com"
+ */
+router.get('/patients', PatientController.retrieveList)
+
+
 router
   .post('/patients', PatientController.create)
-  .get('/patients', PatientController.retrieveList)
   .get('/patients/:uuid', PatientController.retrieveByUUID)
   .patch('/patients/:uuid', PatientController.updateByUUID)
   .delete('/patients/:uuid', PatientController.deleteByUUID)
+
 
 module.exports = router
