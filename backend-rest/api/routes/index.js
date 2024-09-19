a6e4a49c53340eb810d38df4d723fbda9d4359f6
jtonyNet <jtony.net@gmail.com>
2024-09-18 23:01:19 -0300

    feat: fluxo de autenticacao SIMPLES de doctor

--- backend-rest/api/routes/index.js
+++ backend-rest/api/routes/index.js
@@ -1,12 +1,13 @@
 const bodyParser = require('body-parser')
 
 const patient = require('./patientRoutes')
-const doctor = require('./doctorRoutes')
+const auth = require('./authRoutes')
+// const doctor = require('./doctorRoutes') UNDOCUMENTED
 
 module.exports = app => {
   app.use(
     bodyParser.json(),
     patient,
-    doctor
+    auth
   )
 }
